22a55ba8bdb310a2d532a3df06128edae676b0d2
Javier Lozano <javier_l_o@hotmail.com>
2018-08-13 17:41:17 -0500

    Se configura las rutas y el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot( ROUTES, {useHash:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
