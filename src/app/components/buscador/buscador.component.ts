40af328f1da7220a2f8918366f9a015a695f6dd7
Javier Lozano <javier_l_o@hotmail.com>
2018-08-13 23:03:29 -0500

    se inicia el buscador, se recibe artista de spotify

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
   styles: []
 })
-export class BuscadorComponent implements OnInit {
+export class BuscadorComponent  {
 
-  constructor() { }
+  artistas: any [] = [];
 
-  ngOnInit() {
+  constructor( private spotify: SpotifyService) { }
+
+  buscar( termino: string) {
+    this.spotify.getArtistas(termino).subscribe((data: any) => {
+      console.log(data.artists.items);
+      this.artistas = data.artists.items;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,9 +12,17 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCqZPXU5HiHQuCA-1-THL9pVYfPuOjfaF6-dFqi6ON8ysInibW3O9G9oTqgszGidcHazRsa-bckqI6DKKk'
+      'Authorization': 'Bearer BQAgELHYWMn6RIDoIHp8_yM_ksdOxaAOJSXbCmy0tH1HIyfp3kQmQ3eoJWO01hlNAJUc9AhUAwjY_HSmFz8'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
+  getArtistas( termino: string) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQAgELHYWMn6RIDoIHp8_yM_ksdOxaAOJSXbCmy0tH1HIyfp3kQmQ3eoJWO01hlNAJUc9AhUAwjY_HSmFz8'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 }
