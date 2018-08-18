f37e89346d28223721aa914cc6e2dedefd994000
Javier Lozano <javier_l_o@hotmail.com>
2018-08-18 16:50:49 -0500

    se agrega map a los observables del servicio

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -11,18 +12,20 @@ export class SpotifyService {
 
   getNewReleases() {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAgELHYWMn6RIDoIHp8_yM_ksdOxaAOJSXbCmy0tH1HIyfp3kQmQ3eoJWO01hlNAJUc9AhUAwjY_HSmFz8'
+    const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQCx4lHm-nxlEbmGgETmReM9C1b8yMiu3zh0-_S4Y8GywmPAFpUbLcTAghG0noUk9o0E5U6oMaUhBu0qGRc'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+                    .pipe( map (data =>  data['albums'].items) );
   }
   getArtistas( termino: string) {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAgELHYWMn6RIDoIHp8_yM_ksdOxaAOJSXbCmy0tH1HIyfp3kQmQ3eoJWO01hlNAJUc9AhUAwjY_HSmFz8'
+    const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQCx4lHm-nxlEbmGgETmReM9C1b8yMiu3zh0-_S4Y8GywmPAFpUbLcTAghG0noUk9o0E5U6oMaUhBu0qGRc'
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+             .pipe ( map ( data =>  data['artists'].items ));
   }
 }
